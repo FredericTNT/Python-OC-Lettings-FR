11f008270211876f82fadb75231927877945f20b
FredericTNT <frederic.TNT78@gmail.com>
2022-12-19 20:08:58 +0100

    Refactoring 2 applications lettings/profiles v1

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from oc_lettings_site.views import index
+from lettings.views import index as lettings_index, letting
+from profiles.views import index as profiles_index, profile
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('', index, name='index'),
+    path('lettings/', lettings_index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', letting, name='letting'),
+    path('profiles/', profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
