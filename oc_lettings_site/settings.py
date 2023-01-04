cae31ba52e29b6aec99cd973b7e3ce48c63b062d
FredericTNT <frederic.TNT78@gmail.com>
2023-01-04 16:54:32 +0100

    Pipelines CI/CD v3.2.2

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -108,9 +108,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR.joinpath('static/'),
-]
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
