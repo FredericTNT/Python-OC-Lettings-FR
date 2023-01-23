60b8d60121705bbc22b26a8488487bad3eb97b7b
FredericTNT <frederic.TNT78@gmail.com>
2023-01-23 16:21:37 +0100

    Sentry init v1.2

--- lettings/urls.py
+++ lettings/urls.py
@@ -2,12 +2,8 @@ from django.urls import path
 from lettings.views import index, letting
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
 app_name = 'lettings'
 urlpatterns = [
     path('', index, name='index'),
     path('<int:letting_id>/', letting, name='letting'),
-    path('sentry-debug/', trigger_error),
 ]
